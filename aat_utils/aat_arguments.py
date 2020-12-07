116e747088a2e1cb70256e58fc94dc90f9875249
Mr Froggo <75422558+MrFrogGuy@users.noreply.github.com>
2020-12-07 18:56:21 -0500

    Update aat_arguments.py

--- aat_utils/aat_arguments.py
+++ aat_utils/aat_arguments.py
@@ -7,13 +7,15 @@ l_args = [
 	[fg.YELLOW, fg.RESET]
 ] # For the colorama arguments
 
+aatTFormat = "%H:%M:%S"
+aatLFormat = "%H:%M:%S-%d-%m-%y.log"
 aatPrefix = "aat "  #Bot command prefix Example: aat purge [*args]
 
 
 is_self = lambda msg: True if msg.author == aatClient.user else False
-aerror = lambda msg: print("{}Error\t{}{}] {}".format(*l_args[0], strftime("%H:%M:%S"), msg))
-ainfo = lambda msg: print("{}Info\t{}{}] {}".format(*l_args[1], strftime("%H:%M:%S"), msg))
-awarning = lambda msg: print("{}Warning\t{}{}] {}".format(*l_args[2], strftime("%H:%M:%S"), msg))
+aerror = lambda msg: print("{}Error\t{}{}] {}".format(*l_args[0], strftime(aatTFormat), msg))
+ainfo = lambda msg: print("{}Info\t{}{}] {}".format(*l_args[1], strftime(aatTFormat), msg))
+awarning = lambda msg: print("{}Warning\t{}{}] {}".format(*l_args[2], strftime(aatTFormat), msg))
 
 
 purge_arguments = {
