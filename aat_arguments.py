74581b4c237bd51dddc51d7eb74f1974ca3b8a39
Mr Froggo <75422558+MrFrogGuy@users.noreply.github.com>
2020-12-07 16:12:51 -0500

    Update aat_arguments.py

--- aat_arguments.py
+++ aat_arguments.py
@@ -14,3 +14,14 @@ is_self = lambda msg: True if msg.author == aatClient.user else False
 aerror = lambda msg: print("{}Error\t{}{}] {}".format(*l_args[0], strftime("%H:%M:%S"), msg))
 ainfo = lambda msg: print("{}Info\t{}{}] {}".format(*l_args[1], strftime("%H:%M:%S"), msg))
 awarning = lambda msg: print("{}Warning\t{}{}] {}".format(*l_args[2], strftime("%H:%M:%S"), msg))
+
+
+purge_arguments = {
+	"to_pass": [
+		None, # Limit
+		None, # Before (UTC ex: 1-1-2020)
+		None, # After (UTC ex: 1-1-2020)
+		None, # Around (UTC ex: 1-1-2020)
+		False # Oldest first | If set to True, return messages in oldest->newest order.
+	]
+}
