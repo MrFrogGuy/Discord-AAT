ee0109759415e09f49d0c7b9e01d8e678bdd4d23
Mr Froggo <75422558+MrFrogGuy@users.noreply.github.com>
2020-12-07 19:54:07 -0500

    Update aat_arguments.py

--- aat_utils/aat_arguments.py
+++ aat_utils/aat_arguments.py
@@ -13,6 +13,7 @@ aatPrefix = "aat "  #Bot command prefix Example: aat purge [*args]
 
 
 is_self = lambda msg: True if msg.author == aatClient.user else False
+has_args = lambda lst, data: True if ( len(lst) > lst.index(data) + 1 ) else False
 aerror = lambda msg: print("{}Error\t{}{}] {}".format(*l_args[0], strftime(aatTFormat), msg))
 ainfo = lambda msg: print("{}Info\t{}{}] {}".format(*l_args[1], strftime(aatTFormat), msg))
 awarning = lambda msg: print("{}Warning\t{}{}] {}".format(*l_args[2], strftime(aatTFormat), msg))
